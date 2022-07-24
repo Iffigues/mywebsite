ce1f48230f7f769a236bbcf781bf9e40457d4a0a
Debian <debian@vps683807.ovh.net>
2022-07-24 18:07:25 +0000

    ez

--- grpc/goclient/goclient/main.go
+++ grpc/goclient/goclient/main.go
@@ -1,37 +1,32 @@
 package main
 
-import  (
-	"net/http"
-	"time"
-	"log"
-	"fmt"
-	"io/ioutil"
+import (
+    "fmt"
+    "github.com/gorilla/mux"
+    "log"
+    "net/http"
+    "time"
 )
-
-func Handler(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
-	})
-}
 
 func main() {
 
-	r := http.NewServeMux()
-	webapp := http.FileServer(http.Dir("/build"))
-	r.Handle("/", Handler(webapp))
-	files, err := ioutil.ReadDir("/build")
-	if err != nil {
-	    fmt.Println(err)
-	}
-	for _, f := range files {
-            fmt.Println(f.Name())
-	}
-	srv := &http.Server{
-		Handler:      r,
-		Addr:         ":8080",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-	log.Fatal(srv.ListenAndServe())
+    r := mux.NewRouter()
 
+    r.HandleFunc("/fortune", index).Methods("GET")
+    buildHandler := http.FileServer(http.Dir("build"))
+    r.PathPrefix("/").Handler(buildHandler)
+    srv := &http.Server{
+        Handler:      r,
+        Addr:         ":8080",
+        WriteTimeout: 15 * time.Second,
+        ReadTimeout:  15 * time.Second,
+    }
+
+    fmt.Println("Server started on PORT 8080")
+    log.Fatal(srv.ListenAndServe())
+
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "build/index.html")
 }
